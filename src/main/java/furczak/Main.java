80dd007c3590bd55bba59b58709fe03dbf48a113
furczak <arch.rafal.krawczyk@gmail.com>
2024-09-02 10:59:34 +0200

    main rebuild

--- src/main/java/furczak/Main.java
+++ src/main/java/furczak/Main.java
@@ -1,7 +1,32 @@
 package furczak;
 
+import furczak.calculators.RouteCalculator;
+import furczak.calculators.bruteforce.RecurrentRoutesCalculator;
+import furczak.generators.RoutePointsGenerator;
+import furczak.model.Route;
+import furczak.model.RouteVariants;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        RoutePointsGenerator generator = new RoutePointsGenerator();
+
+
+        List<Integer> simpleAvailablePoints = generator.getSampleRoutePoints();
+        System.out.printf("Points list: %s%n", simpleAvailablePoints);
+
+        RouteVariants simpleRouteVariants = new RouteVariants(new RecurrentRoutesCalculator());
+        simpleRouteVariants.setAvailablePoints(simpleAvailablePoints);
+        simpleRouteVariants.calculate();
+
+        System.out.printf("Results: %s%n", simpleRouteVariants);
+
+        Route bestSimpleRoute = simpleRouteVariants.getBestRoute();
+        System.out.printf("Best route %s%n", bestSimpleRoute);
+
+//        List<Integer> complexRoutePoints = generator.generateRandomRoutePoints(100, 500);
+
     }
 }

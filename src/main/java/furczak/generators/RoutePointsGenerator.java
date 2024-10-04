589c6589c88928e1c460a7d063debfe81f77db7b
furczak <arch.rafal.krawczyk@gmail.com>
2024-10-04 11:41:04 +0200

    improve and test route points genarator

--- src/main/java/furczak/generators/RoutePointsGenerator.java
+++ src/main/java/furczak/generators/RoutePointsGenerator.java
@@ -1,34 +1,57 @@
 package furczak.generators;
 
-import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.*;
 
-@AllArgsConstructor
 public class RoutePointsGenerator {
 
     private final Random random = new Random();
 
-    private final Map<Integer, List<Integer>> simpleLists = Map.of(
+    @Getter
+    private final Map<Integer, List<Integer>> sampleLists = new HashMap<>(Map.of(
             1, List.of(0, 43, 59, 64, 77, 90, 110, 115, 124, 142, 161, 183)
-    );
+    ));
 
 
-    public List<Integer> getSampleRoutePoints(int number) {
-        if (!simpleLists.containsKey(number)) {
-            Set<Integer> options = simpleLists.keySet();
+    public List<Integer> getSampleRoutePoints(int key) {
+        if (!sampleLists.containsKey(key)) {
+            Set<Integer> options = sampleLists.keySet();
             throw new NoSuchElementException(String.format("Available options for sample route points generator are: %s", options));
         }
-        return simpleLists.get(number);
+        return sampleLists.get(key);
     }
 
+    public void addSampleRoutPoints(int key, List<Integer> sample) {
+        this.sampleLists.put(key, sample);
+    }
+
+    /**
+     * Generates a random sequence of route points between the start point (0) and the specified end point.
+     *
+     * @param numberOfPoints the number of random intermediate points to generate between the start point and end point.
+     *                       If this value is 0 or less, no random points will be added.
+     * @param endPoint the destination point, which must be greater than 1.
+     * @return a list of points starting with 0, containing random intermediate points (if any), and ending with {@code endPoint}.
+     *         The list is sorted in ascending order. Points can have duplicates.
+     * @throws IllegalArgumentException if {@code endPoint} is less than or equal to 1.
+     */
     public List<Integer> generateRandomRoutePoints(int numberOfPoints, int endPoint) {
+        endPointValidator(endPoint);
         List<Integer> randomRoutePoints = new ArrayList<>();
+
+        randomRoutePoints.add(0);
         for (int i = 0; i < numberOfPoints; i++) {
             randomRoutePoints.add(random.nextInt(1, endPoint));
         }
-        randomRoutePoints.add(0);
         randomRoutePoints.add(endPoint);
+
         return randomRoutePoints.stream().sorted().toList();
+    }
+
+    private void endPointValidator(int endPoint) {
+        if (endPoint <= 1) {
+            throw new IllegalArgumentException("End point must be greater than 1");
+        }
     }
 }
---
+++ src/test/java/furczak/generators/RoutePointsGeneratorTest.java
@@ -0,0 +1,105 @@
+package furczak.generators;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.InstanceOfAssertFactories;
+import org.assertj.core.api.InstanceOfAssertFactory;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+
+class RoutePointsGeneratorTest {
+
+    private final RoutePointsGenerator generator = new RoutePointsGenerator();
+
+    @Test
+    void getSampleRoutePoints_shouldReturnRequestPointList() {
+        //given:
+        int key = 1;
+        List<Integer> sampleList = generator.getSampleLists().get(key);
+        int listSize = sampleList.size();
+
+        //when:
+        List<Integer> actual = generator.getSampleRoutePoints(key);
+
+        //then:
+        Assertions.assertThat(actual)
+                .isNotEmpty()
+                .hasSize(listSize)
+                .isEqualTo(sampleList);
+    }
+
+    @Test
+    void getSampleRoutePoints_shouldThrowNoSuchElementException() {
+        //given:
+        int key = 0;
+        List<Integer> sampleList = generator.getSampleLists().get(key);
+
+        //when&than:
+        Assertions.assertThatThrownBy(()-> generator.getSampleRoutePoints(key))
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    void addSampleRoutPoints_shouldAddNewList() {
+        //given:
+        int key = 111;
+        List<Integer> testList = List.of(23, 44, 57);
+
+        //when:
+        generator.addSampleRoutPoints(key,testList);
+        Map<Integer, List<Integer>> actual = generator.getSampleLists();
+
+        //then:
+        Assertions.assertThat(actual)
+                .containsKey(key)
+                .extractingByKey(key, InstanceOfAssertFactories.list(Integer.class))
+                .hasSize(testList.size())
+                .containsExactlyElementsOf(testList);
+    }
+
+    @Test
+    void generateRandomRoutePoints_shouldReturnRequestedList() {
+        //given:
+        int listLength = 10;
+        int lastNumber= 111;
+
+        //when:
+        List<Integer> actual = generator.generateRandomRoutePoints(listLength, lastNumber);
+
+        //then:
+        Assertions.assertThat(actual)
+                .isNotEmpty()
+                .hasSize(listLength+2)
+                .contains(0,lastNumber);
+    }
+
+    @Test
+    void generateRandomRoutePoints_shouldReturnTwoElementsList() {
+        //given:
+        int listLength = 0;
+        int lastNumber= 111;
+
+        //when:
+        List<Integer> actual = generator.generateRandomRoutePoints(listLength, lastNumber);
+
+        //then:
+        Assertions.assertThat(actual)
+                .isNotEmpty()
+                .hasSize(2)
+                .containsExactly(0,lastNumber);
+    }
+
+    @Test
+    void generateRandomRoutePoints_shouldThrowIllegalArgumentException() {
+        //given:
+        int listLength = 10;
+        int lastNumber= 1;
+
+        //when & then:
+       Assertions.assertThatThrownBy(()->generator.generateRandomRoutePoints(listLength, lastNumber))
+               .isInstanceOf(IllegalArgumentException.class);
+    }
+}

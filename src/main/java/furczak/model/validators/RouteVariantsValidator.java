4335b28abee26afbe0b116de5116922ae6c5ab8b
furczak <arch.rafal.krawczyk@gmail.com>
2024-10-14 18:32:14 +0200

    add validators with tests

---
+++ src/main/java/furczak/model/validators/RouteVariantsValidator.java
@@ -0,0 +1,23 @@
+package furczak.model.validators;
+
+import java.util.List;
+
+public class RouteVariantsValidator {
+    public void validateDistances(int min, int max) {
+        if (min <= 0 || max <= 0) {
+            throw new IllegalArgumentException("Route distances must be greater then 0");
+        }
+        if (min > max) {
+            throw new IllegalArgumentException("Max value must be greater or equal to min value");
+        }
+    }
+
+    public void validateAvailablePoints(List<Integer> availablePoints) {
+        if (availablePoints.isEmpty()) {
+            throw new IllegalArgumentException("List of available points is empty");
+        }
+        if (availablePoints.stream().anyMatch(point -> point <= 0)) {
+            throw new IllegalArgumentException("List of available points cannot contain values less or equal to 0");
+        }
+    }
+}
---
+++ src/test/java/furczak/model/validators/RouteVariantsValidatorTest.java
@@ -0,0 +1,83 @@
+package furczak.model.validators;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class RouteVariantsValidatorTest {
+
+    @Test
+    void validateDistances_shouldNotThrowAnyException() {
+        //given:
+        int min = 5;
+        int max = 10;
+        RouteVariantsValidator validator = new RouteVariantsValidator();
+
+        //when and then:
+        Assertions.assertThatCode(() -> validator.validateDistances(min, max))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void validateDistances_shouldThrowIllegalArgumentException_whenMinOrMaxIsLowerThen1() {
+        //given:
+        int min = 0;
+        int max = 10;
+        RouteVariantsValidator validator = new RouteVariantsValidator();
+
+        //when and then:
+        Assertions.assertThatThrownBy(() -> validator.validateDistances(min, max))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Route distances must be greater then 0");
+    }
+
+    @Test
+    void validateDistances_shouldThrowIllegalArgumentException_whenMinIsGreaterThanMax() {
+        //given:
+        int min = 20;
+        int max = 10;
+        RouteVariantsValidator validator = new RouteVariantsValidator();
+
+        //when and then:
+        Assertions.assertThatThrownBy(() -> validator.validateDistances(min, max))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Max value must be greater or equal to min value");
+    }
+
+    @Test
+    void validateAvailablePoints_shouldNotThrowAnyException() {
+        //given:
+        List<Integer> points = List.of(23,45,67);
+        RouteVariantsValidator validator = new RouteVariantsValidator();
+
+        //when and then:
+        Assertions.assertThatCode(() -> validator.validateAvailablePoints(points))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    void validateAvailablePoints_shouldThrowIllegalArgumentException_whenPointListIsEmpty() {
+        //given:
+        List<Integer> points = new ArrayList<>();
+        RouteVariantsValidator validator = new RouteVariantsValidator();
+
+        //when and then:
+        Assertions.assertThatThrownBy(() -> validator.validateAvailablePoints(points))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("List of available points is empty");
+    }
+
+    @Test
+    void validateAvailablePoints_shouldThrowIllegalArgumentException_whenThereAreAnyPointsLowerThan1() {
+        //given:
+        List<Integer> points = List.of(0,23,45);
+        RouteVariantsValidator validator = new RouteVariantsValidator();
+
+        //when and then:
+        Assertions.assertThatThrownBy(() -> validator.validateAvailablePoints(points))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("List of available points cannot contain values less or equal to 0");
+    }
+}

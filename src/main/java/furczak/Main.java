b8edd6cc1e7bf8189964a519a2569c3858c14408
furczak <arch.rafal.krawczyk@gmail.com>
2024-10-28 17:38:33 +0100

    add variables to main

--- src/main/java/furczak/Main.java
+++ src/main/java/furczak/Main.java
@@ -11,14 +11,27 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
+        // sample setup
+        int sampleNumber = 1;
+        boolean generate = false;
+        int numerOfPoints = 50;
+        int startPoint = 0;
+        int endPoint = 400;
+
+        // calculator setup
+        int minDistance = 22;
+        int maxDistance = 44;
+        var calculator = new RecurrentRoutesCalculator();
+
+
         RoutePointsGenerator generator = new RoutePointsGenerator();
-
-
-        List<Integer> availablePoints = generator.getSampleRoutePoints(1);
-//        List<Integer> availablePoints = generator.generateRandomRoutePoints(50,0, 400);
+        List<Integer> availablePoints = generator.getSampleRoutePoints(sampleNumber);
+        if (generate) {
+            availablePoints = generator.generateRandomRoutePoints(numerOfPoints, startPoint, endPoint);
+        }
         System.out.printf("Points list: %s%n", availablePoints);
 
-        RouteVariants simpleRouteVariants = new RouteVariants(new RecurrentRoutesCalculator(), 22, 44);
+        RouteVariants simpleRouteVariants = new RouteVariants(calculator, minDistance, maxDistance);
         simpleRouteVariants.setAvailablePoints(availablePoints);
 
         long start = System.currentTimeMillis();
@@ -27,7 +40,7 @@ public class Main {
 
         System.out.printf("%nResults: %s%n", simpleRouteVariants);
 
-        System.out.printf("Calculation time: %dms%n", end-start);
+        System.out.printf("Calculation time: %dms%n", end - start);
 
         StageRoute bestSimpleStageRoute = simpleRouteVariants.getBestRoute();
         System.out.printf("Best route %s%n", bestSimpleStageRoute);
